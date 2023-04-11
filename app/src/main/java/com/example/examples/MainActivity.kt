dd2ba193a39103f5bed1933c0500d461c60c5aab
SunYoung <baby_ash@naver.com>
2023-04-11 20:34:50 +0900

    navigation

--- app/src/main/java/com/example/examples/MainActivity.kt
+++ app/src/main/java/com/example/examples/MainActivity.kt
@@ -17,27 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ExamplesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ExamplesTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/examples/Navigation.kt
@@ -0,0 +1,68 @@
+package com.example.examples
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "?name={name}",
+            arguments = listOf(navArgument("name") {
+                type = NavType.StringType
+                defaultValue = "Ann"
+                nullable = true
+            })
+        ) { entry ->
+            DetailScreen(name = entry.arguments?.getString("name"))
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(50.dp)
+    ) {
+        TextField(value = text, onValueChange = {
+            text = it
+        }, Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { navController.navigate(Screen.DetailScreen.withArgs(text)) },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To Detail Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(name: String?) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Hello $name")
+    }
+}
